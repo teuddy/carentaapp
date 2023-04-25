c0fb2ed8a6b88b3d0ca0fed3ea470a4ffcbdad2f
Jarvis <jarvis@MacBook-Air-de-teuddy.local>
2023-04-25 01:10:32 -0400

    fixing cors

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 // server.js
 // import { createServer } from "http";
 import express from 'express'
+import cors from 'cors'
 // const express = require('express')
 const next = require('next')
 const dev = process.env.NODE_ENV !== 'production';
@@ -19,14 +20,30 @@ const nextHandler = nextApp.getRequestHandler();
 
 const app = express()
 
+let allowedOrigin = "https://carentaapp.vercel.app";
+
+const corsOptions = {
+    origin: allowedOrigin,
+    credentials: true,
+  };
+  
+app.use(cors(corsOptions));
+
 nextApp.prepare().then(() => {
+
+ 
+
+    app.use(cors(corsOptions))
+
     app.get('/api/hello',(req,res)=>{
         res.send("hello worssssld")
     })
     //TODAS LAS RUTAS DEL SERVIDOR
+
     app.use('/api/student/',require('./api/routes/studenRoutes'))
     //RUTAS DE NEXTJS
     app.get('*', (req, res) => nextHandler(req, res));
+
     //SERVIDOR ESCUCHANDO
     app.listen(3000,()=>{
         console.log("SERVER UP")

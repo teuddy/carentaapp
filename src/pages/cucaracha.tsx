12ae5270da70c566e9bf957d08eabc929d382a0b
Jarvis <jarvis@MacBook-Air-de-teuddy.local>
2023-04-25 01:01:51 -0400

    fixing tsc

--- src/pages/cucaracha.tsx
+++ src/pages/cucaracha.tsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
 import useGetStudents from "../hooks/useGetStudents";
 
 // - >  carenta.app -> MAIN * bloqueado -> carenta.app | 
 
+
+type Student ={
+    name : string,
+    age: number,
+    grade: string
+}
+
 const Cucaracha = ()=>{
-    const {data} = useGetStudents()
+    const [students,setStudents] = useState<Student[]>()
+    const query = useGetStudents()
+
+    useEffect(()=>{
+        if(query.isSuccess){
+            setStudents(query.data?.data)
+        }
+
+    },[query.isSuccess])
+
+
     return(
         <div>
             <ul>
-            {data.data.map((student)=>{
-                return <li>{student.name} {student.age} {student.grade}</li>
+            {students && students.map((student,index)=>{
+                return <li key={index}>{student.name} {student.age} {student.grade}</li>
             })}
             </ul>
         </div>

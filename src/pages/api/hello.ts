84e0977a7044d2216a0a6e15dddb4db6d8ace1e4
Jarvis <jarvis@MacBook-Air-de-teuddy.local>
2023-04-26 19:27:16 -0400

    fix

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
+type Student = {
   name: string
+  age: number,
+  grade: string
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Student[]>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json([{
+      name: "Teuddy",
+      age: 20,
+      grade: "a"
+  },
+      {
+        name: "Sebas",
+        age: 20,
+        grade: "a"
+    },
+    {
+      name: "Lucho",
+      age: 20,
+      grade: "a"
+    }])
 }

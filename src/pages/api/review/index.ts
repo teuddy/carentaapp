6c8e72a2f9db4762a45abf44c6c38ea4b2c2f144
LuchoToledo <118233128+LuchoToledo@users.noreply.github.com>
2023-04-27 00:27:14 -0400

    principio de mi review

---
+++ src/pages/api/review/index.ts
@@ -0,0 +1,5 @@
+import type { NextApiRequest, NexApiResponse } from 'next'
+import { connect } from "../../../../utils/dbConnection"
+
+
+import { creataPayment } from "../../../../controllers/"
--- utils/dbConnection.ts
+++ utils/dbConnection.ts
@@ -1,14 +1,25 @@
-import mongoose from "mongoose";
+const mongoose  = require("mongoose");
 
-
-export const connect = ()=>{
+exports.connect = () =>{
     const options = {
         useNewUrlParser: true,
         autoIndex: false, // Don't build indexes
           maxPoolSize: 10, // Maintain up to 10 socket connections
       };
     
-      const connectWithRetry = ()=>{
-        mongoose.Promise 
-      }
-}
+      const connectWithRetry = () => {
+        mongoose.Promise = global.Promise;
+        console.log("MongoDB connection with retry");
+        mongoose
+          .connect("mongodb://127.0.0.1:27017/carenta", options)
+          then(()=> {
+            console.log("MongoDB is connected");
+          })
+          .catch(err) => {
+            console.log("MongoDB connection unsuccessful, retry after 2 seconds.",
+            err);
+            setTimeout(connectWithRetry, 2000);
+          });
+      };
+  connectWithRetry();
+};

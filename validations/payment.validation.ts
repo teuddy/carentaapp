5ffcb9dfcb5a19da7f90af7ac080821252ba6c39
OlmosUY <sebaolmos@gmail.com>
2023-04-27 19:57:30 -0300

    validation fix, object per string

--- validations/payment.validation.ts
+++ validations/payment.validation.ts
@@ -1,9 +1,9 @@
 import Joi from 'joi'
 
 export const paymentSchema = Joi.object({
-    reservation_id: Joi.object().required(),
-    user_id: Joi.object().required(),
-    listing_id: Joi.object().required(),
+    reservation_id: Joi.string().required(),
+    user_id: Joi.string().required(),
+    listing_id: Joi.string().required(),
     amount: Joi.number().required(),
     payment_date: Joi.date().required(),
     payment_method: Joi.string().required()

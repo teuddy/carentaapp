4cfcf864f969b622a9531cd30a3ffc526e838848
OlmosUY <sebaolmos@gmail.com>
2023-04-27 01:01:06 -0300

    create connection to the DataBase

--- src/pages/api/payment/index.ts
+++ src/pages/api/payment/index.ts
@@ -1,17 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { connect } from '../../../../utils/dbConnection'
 // import { create as CreateListingController } from '../../../../controllers/listing.controller'
 import { createPayment } from '../../../../controllers/payment.controller'
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
     ) {
     
+    await connect();
+
     if (req.method === 'POST') {
         createPayment(req,res)
     } else {
         res.send('hola')
     }
-
-        
 }

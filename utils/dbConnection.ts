050afec6c88dbad36d5577e93553de0339b2cf92
OlmosUY <sebaolmos@gmail.com>
2023-05-02 15:04:18 -0300

    Indentation correction

--- utils/dbConnection.ts
+++ utils/dbConnection.ts
@@ -6,7 +6,7 @@ export const connect = () =>{
   const options = {
     useNewUrlParser: true,
     autoIndex: false, // Don't build indexes
-      maxPoolSize: 10, // Maintain up to 10 socket connections
+    maxPoolSize: 10, // Maintain up to 10 socket connections
   };
   const connectWithRetry = () => {
     mongoose.Promise = global.Promise;
